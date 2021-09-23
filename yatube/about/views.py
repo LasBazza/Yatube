59325ba52fe056ac3983768425b872404553186f
IgnatyevDmitriy <ignatyev-ds@yandex.ru>
2021-09-23 22:52:21 +0300

    Add static

--- yatube/about/views.py
+++ yatube/about/views.py
@@ -1,11 +1,6 @@
 from django.views.generic.base import TemplateView
 
 
-class AboutAuthorView(TemplateView):
-    """Show page about author"""
-    template_name = 'about/author.html'
-
-
 class AboutTechView(TemplateView):
     """Show page about technologies"""
     template_name = 'about/tech.html'
