f93a685d37d6ed973976ae2f23499ed58bc230bb
IgnatyevDmitriy <ignatyev-ds@yandex.ru>
2021-09-24 21:44:31 +0300

    fix tests

--- yatube/about/tests/test_views.py
+++ yatube/about/tests/test_views.py
@@ -12,22 +12,12 @@ class AboutPagesTest(TestCase):
 
     def test_pages_use_correct_template(self):
         """URL-адрес использует соответствующий шаблон."""
-        templates_pages_names = {
-            'about/author.html': reverse('about:author'),
-            'about/tech.html': reverse('about:tech'),
-        }
-        for template, reverse_name in templates_pages_names.items():
-            with self.subTest(reverse_name=reverse_name):
-                response = self.guest_client.get(reverse_name)
-                self.assertTemplateUsed(response, template)
+
+        response = self.guest_client.get(reverse('about:tech'))
+        self.assertTemplateUsed(response, 'about/tech.html')
 
     def test_static_pages_exist_at_desired_location(self):
-        """Страницы /about/author и /about/tech доступны любому пользователю"""
-        pages = (
-            '/about/author/',
-            '/about/tech/',
-        )
-        for url in pages:
-            with self.subTest(url=url):
-                response = AboutPagesTest.guest_client.get(url)
-                self.assertEqual(response.status_code, HTTPStatus.OK)
+        """Страница /about/tech доступны любому пользователю"""
+
+        response = AboutPagesTest.guest_client.get('/about/tech/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
