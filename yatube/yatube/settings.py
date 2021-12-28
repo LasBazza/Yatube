ced83b4aa5a8ec93b16db370d97a57088831c921
IgnatyevDmitriy <ignatyev-ds@yandex.ru>
2021-12-28 13:47:29 +0300

    Changed templates

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -25,15 +25,15 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'posts',
-    'users',
-    'about',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
+    'users',
+    'about',
     'sorl.thumbnail',
     'debug_toolbar',
 ]
@@ -119,7 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
