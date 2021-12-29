863792f703f682c04a96ec26dd897dcc1fa8c691
IgnatyevDmitriy <ignatyev-ds@yandex.ru>
2021-12-29 11:02:52 +0300

    check models

--- posts/models.py
+++ posts/models.py
@@ -98,7 +98,7 @@ class Follow(models.Model):
     )
 
     class Meta:
-        constraints = models.UniqueConstraint(
+        constraints = [models.UniqueConstraint(
             fields=['user', 'author'],
             name='unique_follows'
-        )
+        ), ]
